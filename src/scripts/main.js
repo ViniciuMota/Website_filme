f1f8b7338b53995f392101909cc36a22ba0afb55
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-04-17 21:58:04 -0300

    layout final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const tabButtons = document.querySelectorAll("[data-tab-button]");
+  const moviesTabsButton = document.querySelectorAll("[data-tab-button]");
 
   // programação das abas;
-  for (let i = 0; i < tabButtons.length; i++) {
-    tabButtons[i].addEventListener("click", function (button) {
-      const targetContainer = button.target.dataset.tabButton;
-      const container = document.querySelector(
-        `[data-tab-id=${targetContainer}]`
+  for (let i = 0; i < moviesTabsButton.length; i++) {
+    moviesTabsButton[i].addEventListener("click", function (button) {
+      const moviesListId = button.target.dataset.tabButton;
+      const moviesList = document.querySelector(
+        `[data-tab-id=${moviesListId}]`
       );
-      hideAllSheets();
+      hideAllMoviesList();
+      moviesList.classList.add("movies__list--is-active");
+      removeActiveButtonClass();
+      button.target.classList.add("movies__tabs__button--is-active");
     });
   }
 });
 
-function hideAllSheets() {
-  const tabsContainer = document.querySelectorAll("[data-tab-id");
-  for (let i = 0; i < tabsContainer.length; i++) {
-    tabsContainer[i].classList.remove("movies__list--is-active");
+// remover classe is-active das divs
+function hideAllMoviesList() {
+  const moviesList = document.querySelectorAll("[data-tab-id]");
+
+  for (let i = 0; i < moviesList.length; i++) {
+    moviesList[i].classList.remove("movies__list--is-active");
+  }
+}
+
+// remover classe is-active dos botões
+function removeActiveButtonClass() {
+  const moviesTabsButton = document.querySelectorAll("[data-tab-button]");
+
+  for (let i = 0; i < moviesTabsButton.length; i++) {
+    moviesTabsButton[i].classList.remove("movies__tabs__button--is-active");
   }
 }
